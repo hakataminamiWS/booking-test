e8fd8421525daf29c7184ae37aac02323010a53e
hakataminamiWS <hakataminami.web.service@gmail.com>
2026-01-17 16:00:41 +0900

    feat: Add relationship aliases to the Shop model to support route model binding for related resources.

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -87,4 +87,72 @@ class Shop extends Model
     {
         return $this->hasManyThrough(ShopStaffSchedule::class, ShopStaff::class);
     }
+
+    // ======================================================================
+    // Relationship Aliases for Route Model Binding (scopeBindings)
+    // ======================================================================
+
+    /**
+     * Alias for staffs() to support route parameter {staff}
+     */
+    public function staff(): HasMany
+    {
+        return $this->staffs();
+    }
+
+    /**
+     * Alias for menus() to support route parameter {menu}
+     */
+    public function menu(): HasMany
+    {
+        return $this->menus();
+    }
+
+    /**
+     * Alias for options() to support route parameter {option}
+     */
+    public function option(): HasMany
+    {
+        return $this->options();
+    }
+
+    /**
+     * Alias for bookings() to support route parameter {booking}
+     */
+    public function booking(): HasMany
+    {
+        return $this->bookings();
+    }
+
+    /**
+     * Alias for bookers() to support route parameter {booker}
+     */
+    public function booker(): HasMany
+    {
+        return $this->bookers();
+    }
+
+    /**
+     * Alias for staffApplications() to support route parameter {staff_application}
+     */
+    public function staffApplication(): HasMany
+    {
+        return $this->staffApplications();
+    }
+
+    /**
+     * Alias for shopSpecialOpenDays() to support route parameter {special_open_day}
+     */
+    public function shopSpecialOpenDay(): HasMany
+    {
+        return $this->shopSpecialOpenDays();
+    }
+
+    /**
+     * Alias for shopSpecialClosedDays() to support route parameter {special_closed_day}
+     */
+    public function shopSpecialClosedDay(): HasMany
+    {
+        return $this->shopSpecialClosedDays();
+    }
 }
